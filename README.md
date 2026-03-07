527444484ea0cfa654c5c3672fb5d9bc9e8d8c30
ethan-davies <me@ethann.dev>
2026-03-07 23:16:37 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Soup's Waywall Config
+snoopy
+## Features
+* 1920x1080
+* Uses Esensats' [Waywork](https://github.com/Esensats/waywork) framework
+* Uses Esensats' [floating window helper](https://github.com/Esensats/waywall-floating) for Ninjabrain Bot
+* Uses Saanvi's [plug.waywall](https://github.com/its-saanvi/plug.waywall) module
+* Uses Gore's [music overlay](https://github.com/arjuncgore/ww_music_overlay) plugin for Spotify overlay and controls
+* Uses thin, tall, and wide resolution borders
+* Uses shaders for pie chart and text mirrors
+* Has a normal resolution block entities mirror for [glowdar](https://www.youtube.com/watch?v=8r4icysEvsI)
+* Has a toggle rebind function for chatting
+
+## To-Do
+* Add a Paceman overlay for RSG
+* Use [RSG utils](https://github.com/arjuncgore/ww_rsg_utils) plugin
+* Implement [Lingle](https://github.com/flammablebunny/Lingle) auToMPFS, ADW, and symlinking
+* Add a snoopy cursor
+* Add randomized snoopy backgrounds upon toggling resizes
+* Make a custom ninbot overlay using API services
+* Add [resize animations](https://github.com/char3210/resize_animation)
+* Add a toggleable oneshot crosshair
+* Redo floating ninbot behavior
+* Add backgrounds/borders to text mirrors
---
+++ mcsr.lua
@@ -0,0 +1,407 @@
+-- == imports ==
+local waywall = require("waywall")
+local helpers = require("waywall.helpers")
+local create_floating = require("floating.floating")
+local plug = require("plug.init")
+
+local Scene = require("waywork.scene")
+local Modes = require("waywork.modes")
+local Keys = require("waywork.keys")
+local Processes = require("waywork.processes")
+
+-- == paths ==
+local resources_folder = os.getenv("HOME") .. "/" .. ".config/waywall/resources/"
+
+local eye_overlay_path = resources_folder .. "images/measuring_overlay.png"
+
+local java_path = "/usr/lib/jvm/java-25-openjdk/bin/java"
+local paceman_path = resources_folder .. "jars/paceman-tracker-0.7.1.jar"
+local ninbot_path = resources_folder .. "jars/Ninjabrain-Bot-1.5.1.jar"
+
+-- == read helper function ==
+local read_file = function(name)
+	local file = io.open(name, "r")
+	if file then
+		local data = file:read("*a")
+		file:close()
+		return data
+	end
+end
+
+-- == config variables ==
+local ninbot_anchor, ninbot_opacity = "topright", 0.9
+local normal_sens, tall_sens = 7.66600442, 0.0674594018191557
+local xkb_layout = "danish_craft"
+local keybinds = {
+	enabled = {
+		["mb5"] = "F3",
+		["mb4"] = "backspace",
+		["leftctrl"] = "leftalt",
+		["leftalt"] = "leftctrl",
+		["0"] = "6",
+		["6"] = "0",
+		["CAPSLOCK"] = "KPASTERISK",
+	},
+
+	disabled = {},
+}
+
+local keys = {
+	-- mode toggles
+	thin = "V",
+	tall = "CAPS_LOCK",
+	wide = "grave",
+
+	-- apps
+	toggle_ninbot = "F1",
+	launch_paceman = "Shift-P",
+
+	-- waywall actions
+	fullscreen = "Shift-O",
+	toggle_rebinds = "End",
+}
+
+-- == main config ==
+local config = {
+	input = {
+		layout = xkb_layout,
+		variant = "base",
+		repeat_rate = 150,
+		repeat_delay = 150,
+		remaps = keybinds.enabled,
+		sensitivity = normal_sens,
+		confine_pointer = false,
+	},
+	theme = {
+		background = "#000000",
+		--background_png = bg_path,
+		ninb_anchor = ninbot_anchor,
+		ninb_opacity = ninbot_opacity,
+	},
+	experimental = { debug = false, jit = false, tearing = false, scene_add_text = true },
+	shaders = {
+		["borders"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/borders.frag"),
+		},
+		["pie_chart"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/pie_chart.frag"),
+		},
+		["pie_border"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/pie_border.frag"),
+		},
+		["glowdar"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/glow.frag"),
+		},
+		["glowdar_bg"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/glow_bg.frag"),
+		},
+		["text"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/text.frag"),
+		},
+		["text_bg"] = {
+			vertex = read_file(resources_folder .. "shaders/general.vert"),
+			fragment = read_file(resources_folder .. "shaders/frag/text_bg.frag"),
+		},
+	},
+}
+
+-- == floating controller ==
+local floating = create_floating({
+	show_floating = waywall.show_floating,
+	sleep = waywall.sleep,
+})
+
+-- == scene registry ==
+local scene = Scene.SceneManager.new(waywall)
+
+-- == mirrors ==
+-- = normal =
+-- blockentities
+scene:register("glowdar", {
+	kind = "mirror",
+	options = {
+		src = { x = 1827, y = 858, w = 34, h = 36 },
+		dst = { x = 1667, y = 678, w = 169, h = 179 },
+		shader = "glowdar",
+	},
+	groups = { "normal" },
+})
+scene:register("glowdar_shadow", {
+	kind = "mirror",
+	options = {
+		src = { x = 1827, y = 858, w = 34, h = 36 },
+		dst = { x = 1672, y = 683, w = 169, h = 179 },
+		shader = "glowdar_bg",
+	},
+	groups = { "normal" },
+})
+
+-- = thin =
+-- c and e counter
+scene:register("c_e_counter", {
+	kind = "mirror",
+	options = {
+		src = { x = 1, y = 28, w = 64, h = 18 },
+		dst = { x = 1300, y = 450, w = 320, h = 90 },
+		shader = "text",
+	},
+	groups = { "thin" },
+})
+scene:register("c_e_counter_shadow", {
+	kind = "mirror",
+	options = {
+		src = { x = 1, y = 28, w = 64, h = 18 },
+		dst = { x = 1305, y = 455, w = 320, h = 90 },
+		shader = "text_bg",
+	},
+	groups = { "thin" },
+})
+
+-- o counter
+scene:register("o_counter", {
+	kind = "mirror",
+	options = {
+		src = { x = 45, y = 154, w = 64, h = 10 },
+		dst = { x = 1300, y = 545, w = 320, h = 50 },
+		shader = "text",
+	},
+	groups = { "thin" },
+})
+scene:register("o_counter_shadow", {
+	kind = "mirror",
+	options = {
+		src = { x = 45, y = 154, w = 64, h = 10 },
+		dst = { x = 1305, y = 550, w = 320, h = 50 },
+		shader = "text_bg",
+	},
+	groups = { "thin" },
+})
+
+-- pitch and yaw
+scene:register("pitch_yaw", {
+	kind = "mirror",
+	options = {
+		src = { x = 177, y = 118, w = 64, h = 10 },
+		dst = { x = 1300, y = 600, w = 320, h = 50 },
+		shader = "text",
+	},
+	groups = { "thin" },
+})
+scene:register("pitch_yaw_shadow", {
+	kind = "mirror",
+	options = {
+		src = { x = 177, y = 118, w = 64, h = 10 },
+		dst = { x = 1305, y = 605, w = 320, h = 50 },
+		shader = "text_bg",
+	},
+	groups = { "thin" },
+})
+
+-- pie
+scene:register("pie", {
+	kind = "mirror",
+	options = {
+		src = { x = 0, y = 674, w = 340, h = 178 },
+		dst = { x = 1225, y = 650, w = 315, h = 317.25 },
+		depth = 2,
+		shader = "pie_chart",
+	},
+	groups = { "thin" },
+})
+scene:register("pie_border", {
+	kind = "mirror",
+	options = {
+		src = { x = 0, y = 674, w = 340, h = 178 },
+		dst = { x = 1220, y = 645, w = 325, h = 327.25 },
+		depth = 1,
+		shader = "pie_border",
+	},
+	groups = { "thin" },
+})
+
+-- pie percentages
+scene:register("percentages", {
+	kind = "mirror",
+	options = {
+		src = { x = 247, y = 859, w = 33, h = 25 },
+		dst = { x = 1550, y = 750, w = 132, h = 100 },
+		shader = "text",
+	},
+	groups = { "thin" },
+})
+scene:register("percentages_shadow", {
+	kind = "mirror",
+	options = {
+		src = { x = 247, y = 859, w = 33, h = 25 },
+		dst = { x = 1553, y = 753, w = 132, h = 100 },
+		shader = "text_bg",
+	},
+	groups = { "thin" },
+})
+
+-- = tall =
+-- eye measure mirror
+scene:register("eye_measure", {
+	kind = "mirror",
+	options = {
+		src = { x = 177, y = 7902, w = 30, h = 580 },
+		dst = { x = 30, y = 340, w = 700, h = 400 },
+	},
+	groups = { "tall" },
+})
+
+-- eye measure overlay
+scene:register("eye_overlay", {
+	kind = "image",
+	path = eye_overlay_path,
+	options = { dst = { x = 30, y = 340, w = 700, h = 400 } },
+	groups = { "tall" },
+})
+
+-- == modes ==
+local mode_manager = Modes.ModeManager.new(waywall)
+
+mode_manager:define("normal", {
+	width = 1920,
+	height = 1080,
+})
+
+mode_manager:define("thin", {
+	width = 340,
+	height = 1080,
+	on_enter = function()
+		scene:enable_group("thin", true)
+		scene:enable_group("normal", false)
+	end,
+	on_exit = function()
+		scene:enable_group("thin", false)
+		scene:enable_group("normal", true)
+	end,
+})
+
+mode_manager:define("tall", {
+	width = 384,
+	height = 16384,
+	toggle_guard = function()
+		return not waywall.get_key("F3")
+	end,
+	on_enter = function()
+		scene:enable_group("tall", true)
+		scene:enable_group("normal", false)
+		waywall.set_sensitivity(tall_sens)
+	end,
+	on_exit = function()
+		scene:enable_group("tall", false)
+		scene:enable_group("normal", true)
+		waywall.set_sensitivity(normal_sens)
+	end,
+})
+
+mode_manager:define("wide", {
+	width = 1920,
+	height = 300,
+	on_enter = function()
+		scene:enable_group("wide", true)
+		scene:enable_group("normal", false)
+	end,
+	on_exit = function()
+		scene:enable_group("wide", false)
+		scene:enable_group("normal", true)
+	end,
+})
+
+-- == jar processes ==
+local ensure_ninjabrain =
+	Processes.ensure_java_jar(waywall, java_path, ninbot_path, { "-Dawt.useSystemAAFontSettings=on" })(ninbot_path)
+local ensure_paceman = Processes.ensure_java_jar(waywall, java_path, paceman_path, { "--nogui" })(paceman_path)
+
+-- == action helper booleans ==
+-- = ninbot =
+local is_ninb_ensured = false
+
+-- = remaps =
+local remaps_active = true
+local remaps_text = nil
+
+-- == config actions ==
+local actions = Keys.actions({
+	-- = mode toggles ==
+	[keys.thin] = function()
+		return mode_manager:toggle("thin")
+	end,
+	[keys.tall] = function()
+		return mode_manager:toggle("tall")
+	end,
+	[keys.wide] = function()
+		return mode_manager:toggle("wide")
+	end,
+
+	-- = fullscreen toggle =
+	[keys.fullscreen] = waywall.toggle_fullscreen,
+
+	-- = nbb =
+	[keys.toggle_ninbot] = function()
+		-- ensure ninbot/toggle override
+		if not is_ninb_ensured then
+			ensure_ninjabrain()
+			floating.show()
+			floating.hide_after_timeout(5000)
+			is_ninb_ensured = true
+		else
+			floating.override_toggle()
+		end
+	end,
+	-- temporarily show ninbot on F3+C
+	["*-C"] = function()
+		if waywall.get_key("F3") then
+			waywall.press_key("C")
+			floating.show()
+			floating.hide_after_timeout(30000)
+		else
+			return false
+		end
+	end,
+
+	-- = paceman =
+	[keys.launch_paceman] = function()
+		ensure_paceman()
+	end,
+
+	-- = rebinds toggle =
+	[keys.toggle_rebinds] = function()
+		-- warning text
+		if remaps_text then
+			remaps_text:close()
+			remaps_text = nil
+		end
+		if remaps_active then
+			-- turn off all rebinds
+			remaps_active = false
+			waywall.set_remaps(keybinds.disabled)
+			waywall.set_keymap({ layout = "us" })
+			remaps_text = waywall.text("rebinds off", { x = 100, y = 100, color = "#FFFFFF", size = 2 })
+		else
+			-- turn on all rebinds
+			remaps_active = true
+			waywall.set_remaps(keybinds.enabled)
+			waywall.set_keymap({ layout = xkb_layout })
+		end
+	end,
+})
+
+config.actions = actions
+
+-- == plugins ==
+plug.setup({
+	dir = "plugins",
+	config = config,
+	path = "~/.config/waywall/plugins/",
+})
+
+return config
